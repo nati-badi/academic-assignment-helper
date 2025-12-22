b7e61729678369d6d95503d3cd2a6be5fcd0fdd2
Nati <natibadigojimma@gmail.com>
2025-12-22 22:38:33 +0300

    starting with basic auth endpoints

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, HTTPException
+from schemas import RegisterRequest, LoginRequest
+from security import hash_password, verify_password
+from users import users_db
+from jwt_utils import create_access_token
+
+
+app = FastAPI()
+
+@app.post("/auth/register")
+def register(data: RegisterRequest):
+    # check if user exists
+    for user in users_db:
+        if user["email"] == data.email:
+            raise HTTPException(status_code=400, detail="Email already exists")
+
+    hashed = hash_password(data.password)
+
+    users_db.append({
+        "email": data.email,
+        "password": hashed
+    })
+
+    return {"message": "User registered"}
+
+@app.post("/auth/login")
+def login(data: LoginRequest):
+    for user in users_db:
+        if user["email"] == data.email:
+            if verify_password(data.password, user["password"]):
+                token = create_access_token({
+                    "sub": data.email,
+                    "role": "student"
+                })
+                return {"access_token": token, "token_type": "bearer"}
+
+    raise HTTPException(status_code=401, detail="Invalid credentials")
+
